f5b84cb75d31a9bf619397544615522ac7ccc3fc
freedosss <dima15521@gmail.com>
2020-05-17 19:04:10 +0300

    city list

--- botConfig.js
+++ botConfig.js
@@ -1,23 +1,37 @@
 const { Telegraf } = require("telegraf");
 const Markup = require("telegraf/markup");
 const keyboardDown = require("./helpers/keyboardDown");
-const { btnMenu } = require("./constants/btn");
+const inlineKeyboard = require("./helpers/inlineKeyboard");
+const { btnMenu, genMenu } = require("./constants/btn");
+const studio = require("./db/studio.json");
 
 const bot = new Telegraf(process.env.TOKEN);
 
-bot.start((ctx) =>
+bot.start((ctx) => {
   ctx.reply(
-    `Хей, мы Vean tattoo ❤️\nБренд основан в 2011 году.\nМы несём тату в массы и получаем от этого удовольствие.\nПрисоединяйся к нам!\nЧто тебя интересует ?`,
+    `Хей, мы Vean tattoo ❤️\nБренд основан в 2011 году.\nМы несём тату в массы и получаем от этого удовольствие.\nПрисоединяйся к нам!`,
     keyboardDown(btnMenu)
-  )
-);
+  );
+  ctx.reply("Что тебя интересует ?", inlineKeyboard(genMenu));
+});
 
-// bot.on("message", (ctx) => {
-//   return ctx.reply(
-//     "random ",
-//     Markup.inlineKeyboard([Markup.callbackButton("Test", "test")]).extra() // Work
-//   );
-// });
+// Обработка нижнего меню
+
+bot.hears("Меню", (ctx) => {
+  return ctx.reply("Что тебя интересует ?", inlineKeyboard(genMenu));
+});
+
+bot.hears("Салоны", (ctx) => {
+  ctx.reply(
+    "Выбирите город",
+    inlineKeyboard(
+      // [Markup.callbackButton("Тату", "studio_info")]
+      studio.map((el) => [Markup.callbackButton(el.city, "studio_info")])
+    )
+  );
+});
+
+// Обработка главного меню
 
 bot.action("menu", () => {
   console.log("menu");
--- constants/btn.js
+++ constants/btn.js
@@ -1,4 +1,18 @@
 const Markup = require("telegraf/markup");
 const btnMenu = [["Меню"], ["Салоны"], ["Запись на сеанс"]];
 
-module.exports = { btnMenu };
+const genMenu = [
+  [Markup.callbackButton("Тату", "gen-tatu")],
+  [Markup.callbackButton("Пирсинг", "gen-persing")],
+  [Markup.callbackButton("Татуаж", "gen-tatuj")],
+  [Markup.callbackButton("Удаление", "gen-remove")],
+  [Markup.callbackButton("VeAn SCHOOL", "gen-school")],
+  [
+    {
+      text: "VeAn Магазин",
+      url: "https://vean-shop.com",
+    },
+  ],
+];
+
+module.exports = { btnMenu, genMenu };
